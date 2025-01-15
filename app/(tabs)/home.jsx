8b28e72eb36c1a65bc8871dae364f32a654011d0
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2025-01-15 15:22:33 +0200

    Update home.jsx

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,15 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getForecastDays, restartState, setSearchedLocation  } from "../../Redux/weather/weather";
 import { FontAwesome6 } from "@expo/vector-icons";
 import * as Animatable from 'react-native-animatable';
+
 import { FadeIn } from "react-native-reanimated";
 import Icon from "react-native-vector-icons/Ionicons";
-
-import { useRouter } from 'expo-router';
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { addFavorates, getUser, removeFavorates } from "../../Redux/user/userSlice";
 import { getActions } from "../../Redux/activities/activities";
-
 
 function Home() {
   const [query, setQuery] = useState("South Africa");
@@ -23,12 +20,8 @@ function Home() {
   const dispatch = useDispatch();
   const animatedSwitch = new Animated.Value(0);
   const [favorite, setFavorite] = useState(false);
-
-  const router = useRouter();
-
   const [user , setUser] = useState('')
  useEffect(() => {
-
     dispatch(restartState());
   }, []);
 
@@ -60,7 +53,6 @@ function Home() {
     }).start();
   }, [isSwitchOn]);
 
-  
   const handleSearch = () => {
     if (query.trim()) {
       dispatch(getForecastDays(query));
@@ -97,11 +89,10 @@ function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-
       <Text style={styles.greeting}>Hi, {user || 'User'}</Text>
-
         <Text style={styles.subtitle}>Let's start your vacation!</Text>
       </View>
+      
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.input}
@@ -122,46 +113,6 @@ function Home() {
           <Text style={styles.buttonText}>Search</Text>
         </Pressable>
       </View>
-
-      <View style={styles.content}>
-        <View style={styles.heading}>
-          <View style={styles.location}>
-            <FontAwesome6 name="location-dot" color="#FF914D" size={32} />
-            <Text style={styles.locationName}>{response?.location?.name || query}</Text>
-          </View>
-          <View style={styles.current}>
-            <Text style={styles.today}>Today</Text>
-            <Text style={styles.time}>{response?.location?.localtime.split(" ")[1]}</Text>
-          </View>
-        </View>
-
-        <View style={{ flexDirection: "row", justifyContent: 'space-between', paddingTop: 6 }}>
-          <Animatable.View animation='fadeInLeft' duration={2000} style={{ alignItems: "flex-start" }}>
-            <Text style={{ fontSize: 16, fontWeight: "bold" }}>Feels Like</Text>
-            <View style={{ flexDirection: "row", justifyContent: "flex-start" }}>
-              <Text style={{ fontSize: 46, fontWeight: "bold", width: "100" }}>
-                {response?.current?.feelslike_c}
-              </Text>
-              <Text style={{ fontSize: 14, fontWeight: "bold" }}>C°</Text>
-            </View>
-          </Animatable.View>
-          <Animatable.View animation='fadeInRight' duration={2000} style={{ alignItems: "flex-end" }}>
-            <Image
-              source={{ uri: `https:${response?.current?.condition?.icon}` }}
-              style={{ width: 60, height: 60, backgroundColor: '#F9FAFC' }}
-            />
-            <Text style={{ fontWeight: "bold" }}>{response?.current?.condition?.text}</Text>
-          </Animatable.View>
-        </View>
-
-        <Animatable.View style={{flexDirection:"row" , justifyContent:'space-between', alignItems:'center', paddingVertical:12}}>
-            <Animatable.Text style={{fontSize:16}}>Save to favorite</Animatable.Text>
-            <Pressable style={{paddingRight:6}} onPress={handleAddFavorite}>
-            <FontAwesome6 name={favorite? 'heart-circle-check':'heart' }size={32} color='#FF914D' />
-            </Pressable>
-            
-
-        </Animatable.View>
         <Animatable.View animation='fadeIn' delay={2000} duration={2000} style={[styles.switchContainer, { justifyContent: "space-between" }]}>
           <Pressable
             onPress={() => setIsSwitchOn(false)}
@@ -187,11 +138,50 @@ function Home() {
           </Pressable>
         </Animatable.View>
 
-        {!isSwitchOn ? <FlatList
+     {!isSwitchOn ? <View style={styles.content}>
+        <View style={styles.heading}>
+          <View style={styles.location}>
+            <FontAwesome6 name="location-dot" color="#FF914D" size={32} />
+            <Text style={styles.locationName}>{response?.location?.name || query}</Text>
+          </View>
+          <View style={styles.current}>
+            <Text style={styles.today}>Today</Text>
+            <Text style={styles.time}>{response?.location?.localtime.split(" ")[1]}</Text>
+          </View>
+        </View>
+
+        <View style={{ flexDirection: "row", justifyContent: 'space-between', paddingTop: 6 }}>
+          <Animatable.View animation='fadeInLeft' duration={2000} style={{ alignItems: "flex-start" }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>Feels Like</Text>
+            <View style={{ flexDirection: "row", justifyContent: "flex-start" }}>
+              <Text style={{ fontSize: 46, fontWeight: "bold", width: "100" }}>
+                {response?.current?.feelslike_c}
+              </Text>
+              <Text style={{ fontSize: 14, fontWeight: "bold" }}>C°</Text>
+            </View>
+          </Animatable.View>
+          <Animatable.View animation='fadeInRight' duration={2000} style={{ alignItems: "flex-end" }}>
+            <Image
+              source={{ uri: `https:${response?.current?.condition?.icon}` }}
+              style={{ width: 60, height: 60  , backgroundColor:'#F9FAFC'}}
+            />
+            <Text style={{ fontWeight: "bold" }}>{response?.current?.condition?.text}</Text>
+          </Animatable.View>
+        </View>
+        <Animatable.View style={{flexDirection:"row" , justifyContent:'space-between', alignItems:'center', paddingVertical:12}}>
+            <Animatable.Text style={{fontSize:16}}>Save to favorite</Animatable.Text>
+            <Pressable style={{paddingRight:6}} onPress={handleAddFavorite}>
+            <FontAwesome6 name={favorite? 'heart-circle-check':'heart' }size={32} color='#FF914D' />
+            </Pressable>
+            
+        </Animatable.View>
+
+        <FlatList
           data={response?.forecast?.forecastday}
           style={{ backgroundColor: '#F9FAFC', borderRadius: 18, marginTop: 12 }}
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
+
             <Animatable.View animation='fadeInRight' duration={2000} style={[styles.forecastItem, { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }]}>
               <View style={{ alignItems: "flex-start" }}>
                 <Image source={{ uri: `https:${item?.day?.condition?.icon}` }} width={45} height={45} />
@@ -201,187 +191,196 @@ function Home() {
                 <Text>{item.date}</Text>
                 <Text style={{ fontWeight: 'bold' }}>{item?.day?.avgtemp_c}°C</Text>
               </View>
+
             </Animatable.View>
           )}
-        /> :
-          <Animatable.View
-            animation='fadeInLeft' duration={2000}
-            style={{
-              backgroundColor: "#F9FAFC",
-              padding: 16,
-              borderRadius: 18,
-              marginTop: 12,
-              flex: 1,
-            }}
-          >
-            <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 12 }}>
-              Recommended Activities based on the weather
-            </Text>
-            <View>
-              <View style={styles.activityItem}>
-                <Icon name="water-outline" size={24} style={{ width: '24' }} color="#FF914D" />
-                <Text style={styles.activityText}>Drink Lots of Water</Text>
-              </View>
-              <View style={styles.activityItem}>
-                <Icon name="leaf-outline" size={24} style={{ width: '24' }} color="#FF914D" />
-                <Text style={styles.activityText}>Stay Hydrated</Text>
-              </View>
-              <View style={styles.activityItem}>
-                <Icon name="swim-outline" size={24} style={{ width: '24' }} color="#FF914D" />
-                <Text style={styles.activityText}>Swim</Text>
-              </View>
-            </View>
-          </Animatable.View>}
+        /> 
       </View>
+      :
+         <Animatable.View
+  animation="fadeInLeft"
+  duration={2000}
+  style={{
+    backgroundColor: "#F9FAFC",
+    padding: 16,
+    borderRadius: 18,
+    marginTop: 12,
+    flex: 1,
+  }}
+>
+  <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 12 }}>
+    Recommended Activities based on the weather
+  </Text>
+
+  {Activities?.response?.activities.length === 0 ? (
+    <Animatable.View>
+      <Text>Sorry, we are still working on more recommendations. Try another location.</Text>
+    </Animatable.View>
+  ) : (
+    <FlatList
+      data={Activities?.response.activities || []}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item }) =>
+        response?.current?.condition?.text.includes(item.condition) ? (
+          <Animatable.View animation="fadeIn" duration={2000} style={styles.activityItem}>
+            <FontAwesome6 name="check-circle" size={24} color="#FF914D" />
+            <Text style={styles.activityText}>{item.activity}</Text>
+          </Animatable.View>
+        ) : null // Don't render anything if condition doesn't match
+      }
+    />
+  )}
+</Animatable.View>
+}
     </View>
-     );
-    }
+  );
+}
 
 const styles = StyleSheet.create({
-      container: {
-        padding: 20,
-        flex: 1,
-        backgroundColor: "#F9FAFC",
-      },
-      loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-      header: {
-        marginBottom: 24,
-      },
-      greeting: {
-        fontSize: 48,
-        fontWeight: "bold",
-        color: "#333",
-        marginBottom: 6,
-      },
-      subtitle: {
-        fontSize: 18,
-        color: "#555",
-      },
-      searchContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        borderWidth: 1,
-        borderRadius: 34,
-        borderColor: "#E8DDD2",
-        marginVertical: 12,
-      },
-      input: {
-        flex: 1,
-        padding: 12,
-        fontSize: 16,
-        color: "#333",
-      },
-      current: {
-        alignItems: 'flex-end'
-      },
-      content: {
-        flex: 1,
-        backgroundColor: "#fff",
-        padding: 12,
-        borderRadius: 18,
-      },
-      weatherInfo: {
-        fontSize: 16,
-        color: "#333",
-        textAlign: "center",
-        marginTop: 20,
-      },
-      error: {
-        color: "red",
-        fontSize: 16,
-        textAlign: "center",
-        marginTop: 20,
-      },
-      buttons: {
-        flexDirection: "row",
-        justifyContent: "center",
-      },
-      buttonPrimary: {
-        backgroundColor: "#FF914D",
-        paddingVertical: 12,
-        paddingHorizontal: 25,
-        borderRadius: 30,
-        marginLeft: 10,
-      },
-      buttonText: {
-        color: "#fff",
-        fontSize: 16,
-        fontWeight: "bold",
-      },
-      heading: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
-        width: "100%",
-      },
-      location: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 6,
-      },
-      locationName: {
-        fontSize: 32,
-        fontWeight: "bold",
-      },
-      today: {
-        fontSize: 24,
-        fontWeight: "bold",
-      },
-      switchContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        marginVertical: 20,
-        borderWidth: 1,
-        borderColor: "#FF914D",
-        borderRadius: 23,
-      },
-      switchText: {
-        fontSize: 18,
-        fontWeight: "bold",
-      },
-      switchLabel: {
-        fontSize: 16,
-        color: "#555",
-      },
-      switch: {
-        transform: [{ translateX: 0 }],
-        padding: 5,
-        borderRadius: 20,
-        paddingHorizontal: 10,
-        width: '50%'
-      },
-      forecastItem: {
-        padding: 14,
-        marginBottom: 12,
-        borderRadius: 18,
-        justifyContent: 'space-between',
-        borderColor: '#FF914D',
-        borderWidth: 2
-      },
-      forecastText: {
-        fontSize: 16,
-        color: "#333",
-      },
-      activityItem: {
-        flexDirection: "row",
-        alignItems: "center",
-        marginBottom: 8,
-        borderColor: '#FF914D',
-        borderWidth: 1,
-        padding: 8,
-        borderRadius: 8,
-        backgroundColor: '#fff'
-      },
-      activityText: {
-        fontSize: 16,
-        marginLeft: 8,
-      }
-    });
+  container: {
+    padding: 20,
+    flex: 1,
+    backgroundColor: "#F9FAFC",
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    marginBottom: 24,
+  },
+  greeting: {
+    fontSize: 48,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "#555",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 34,
+    borderColor: "#E8DDD2",
+    marginVertical: 12,
+  },
+  input: {
+    flex: 1,
+    padding: 12,
+    fontSize: 16,
+    color: "#333",
+  },
+  current:{
+    alignItems:'flex-end'
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 12,
+    borderRadius: 18,
+  },
+  weatherInfo: {
+    fontSize: 16,
+    color: "#333",
+    textAlign: "center",
+    marginTop: 20,
+  },
+  error: {
+    color: "red",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 20,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonPrimary: {
+    backgroundColor: "#FF914D",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 30,
+    marginLeft: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  heading: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    width: "100%",
+  },
+  location: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+  },
+  locationName: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  today: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 20,
+    borderWidth: 1,
+    borderColor: "#FF914D",
+    borderRadius: 23,
+  },
+  switchText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  switchLabel: {
+    fontSize: 16,
+    color: "#555",
+  },
+  switch: {
+    transform: [{ translateX: 0 }],
+    padding: 5,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    width: '50%'
+  },
+  forecastItem: {
+    padding: 14,
+    marginBottom: 12,
+    borderRadius: 18,
+    justifyContent: 'space-between',
+    borderColor: '#FF914D',
+    borderWidth: 2
+  },
+  forecastText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  activityItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 8,
+    borderColor: '#FF914D',
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: '#fff'
+  },
+  activityText: {
+    fontSize: 16,
+    marginLeft: 8,
+  }
+});
 
-  export default Home;
+export default Home;
