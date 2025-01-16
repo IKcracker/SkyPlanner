6886cac0e4f2fc8c998fb34530dc12a8ae6e6932
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2025-01-16 04:32:38 +0200

    fix the activities bugs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,44 @@
-import { View } from "react-native";
+import React from "react";
 import { Tabs } from "expo-router";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
 function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ headerShown: false }} />
-      <Tabs.Screen name="profile" options={{ headerShown: false }} />
-      <Tabs.Screen name="map" options={{ headerShown: false }} />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: { backgroundColor: "#fff" }, // Customize the tab bar background
+        tabBarShowLabel: false, // Hide tab labels
+        tabBarActiveTintColor: "#FF914D", // Active tab color
+        tabBarInactiveTintColor: "#999", // Inactive tab color
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" color={color} size={size || 24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" color={color} size={size || 24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="map" color={color} size={size || 24} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
